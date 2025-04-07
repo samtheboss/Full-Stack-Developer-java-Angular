c0aabc9f2e0b95d385a8ef69dae47cf7b590329d
SmartApps <sammykagichu@gmail.com>
2025-04-07 10:15:24 +0300

    create new token on login

--- FrontEnd/src/app/components/login/login.component.ts
+++ FrontEnd/src/app/components/login/login.component.ts
@@ -1,14 +1,14 @@
-import {Component} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {Router} from '@angular/router';
-import {AuthService} from 'src/app/service/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/service/auth.service';
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
     loginForm: FormGroup;
 
@@ -16,30 +16,31 @@ export class LoginComponent {
         private service: AuthService,
         private fb: FormBuilder,
         private router: Router
-    ) {
-    }
+    ) { }
 
     ngOnInit() {
+        // Clear old token if any
+        localStorage.removeItem('JWT');
+
         this.loginForm = this.fb.group({
             username: ['', Validators.required],
             password: ['', Validators.required],
-        })
+        });
     }
 
     login() {
-        const {username, password} = this.loginForm.value;
+        if (this.loginForm.invalid) return;
+
+        const { username, password } = this.loginForm.value;
         this.service.login(username, password).subscribe({
             next: (response) => {
-                localStorage.removeItem('JWT');
                 const jwtToken = response.jwt_token;
                 localStorage.setItem('JWT', jwtToken);
                 this.router.navigate(['dashboard']);
             },
             error: (err) => {
-                console.error('Login failed', err)
+                console.error('Login failed', err);
             }
         });
     }
-
-
 }
--- backend/src/main/java/org/smart/backend/service/JWTService.java
+++ backend/src/main/java/org/smart/backend/service/JWTService.java
@@ -38,7 +38,7 @@ public class JWTService {
                 .add(claims)
                 .subject(username)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 60 * 60 * 1000)) 
+                .expiration(new Date(System.currentTimeMillis() + 60 * 60 * 1000))
                 .and()
                 .signWith(signingKey)
                 .compact();
